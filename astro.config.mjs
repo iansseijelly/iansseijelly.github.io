5186eaea7656e4a8b5de77678320aeec678eee21
Chengyi Lux Zhang <iansseijelly@berkeley.edu>
2025-04-21 12:04:19 -0700

    ADD: config updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
-  integrations: [mdx(), sitemap(), tailwind()]
-});
+  site: 'https://iansseijelly.github.io',
+  base: '/astrofy', // Remove if deploying to a custom domain or using username.github.io repo
+})
